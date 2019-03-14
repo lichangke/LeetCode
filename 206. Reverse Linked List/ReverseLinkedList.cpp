9469e8f69bafa15570df6ac4d74a57b7b0f15a0b
CK-Li <986740304@qq.com>
2019-03-14 22:55:25 +0800

    Create ReverseLinkedList.cpp
    ReverseLinkedList C++ implementation

---
+++ 206. Reverse Linked List/ReverseLinkedList.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode cur = head; //当前指针节点
+        ListNode prev = null; //前指针节点
+        ListNode tmp = null;//临时节点
+        while(cur!=null){
+            tmp = cur.next;//临时节点，暂存当前节点的下一节点，用于后移
+            cur.next = prev; //将当前节点指向它前面的节点
+            prev =  cur;//前指针后移
+            cur = tmp;//当前指针后移
+        }
+        return prev;
+    }
+}
