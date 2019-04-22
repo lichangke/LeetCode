2cac1ccff5f883a0a8875df35cd86615b17942e0
lichangke <986740304@qq.com>
2019-04-23 00:27:49 +0800

    create 70. Climbing Stairs

---
+++ 70. Climbing Stairs/Climbing Stairs_1.py
@@ -0,0 +1,11 @@
+#@author:leacoder
+#@des:  动态规划  爬楼梯
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dicttmp = {0:1,1:1}
+        if n == 1 or n == 0:
+            return dicttmp[n]
+        for i in range(2,n+1):
+            dicttmp[i] = dicttmp[i-1] + dicttmp[i-2]
+        return dicttmp[n]
